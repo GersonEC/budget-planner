4c52e87500c630ad56f717cfc01584e565b3c9cc
GersonEC <gersonenriquez94@gmail.com>
2024-10-25 14:04:23 +0200

    implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { MonthlyBudgetProvider } from './context/MonthlyBudgetContext';
 import { CategoriesProvider } from './context/CategoriesContext';
 import './App.css';
+import { ThemeProvider } from './context/ThemeContext';
+import { ModeToggle } from './components/ModeToggle';
 
 interface AppProps {
   children: React.ReactNode;
@@ -9,11 +11,16 @@ interface AppProps {
 
 const App: React.FC<AppProps> = ({ children }) => {
   return (
-    <MonthlyBudgetProvider>
-      <CategoriesProvider>
-        <div className='App'>{children}</div>
-      </CategoriesProvider>
-    </MonthlyBudgetProvider>
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <MonthlyBudgetProvider>
+        <CategoriesProvider>
+          <div className='App'>
+            <ModeToggle />
+            {children}
+          </div>
+        </CategoriesProvider>
+      </MonthlyBudgetProvider>
+    </ThemeProvider>
   );
 };
 
