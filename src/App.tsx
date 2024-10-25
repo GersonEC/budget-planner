9a91aa03b7ec59c5e74af80262c1ba55afd42f85
GersonEC <gersonenriquez94@gmail.com>
2024-10-25 14:54:03 +0200

    redirect to bills route when there is data on session storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MonthlyBudgetProvider } from './context/MonthlyBudgetContext';
 import { CategoriesProvider } from './context/CategoriesContext';
 import './App.css';
 import { ThemeProvider } from './context/ThemeContext';
 import { ModeToggle } from './components/ModeToggle';
+import { useNavigate } from '@tanstack/react-router';
 
 interface AppProps {
   children: React.ReactNode;
 }
 
 const App: React.FC<AppProps> = ({ children }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const categoriesInSessionStorage = sessionStorage.getItem('categories');
+    const budgetInSessionStorage = sessionStorage.getItem('monthlyBudget');
+    if (categoriesInSessionStorage && budgetInSessionStorage) {
+      navigate({
+        to: '/bills',
+      });
+    }
+  }, [navigate]);
+
   return (
     <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
       <MonthlyBudgetProvider>
