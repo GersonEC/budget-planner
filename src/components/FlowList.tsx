499b7dbda809145f48b8573d5089d74fc0d76af5
GersonEC <gersonenriquez94@gmail.com>
2024-11-06 18:59:23 +0100

    refactor ui

--- src/components/FlowList.tsx
+++ src/components/FlowList.tsx
@@ -22,10 +22,14 @@ export const FlowList: React.FC<Props> = ({ type }) => {
     <ul>
       {flowList.flows.map((flow) => (
         <li key={flow.name}>
-          {flow.name} - {currencyFormat(flow.quantity)}
+          <span className=' text-indigo-300'>{flow.name}</span>:{' '}
+          {currencyFormat(flow.quantity)}
         </li>
       ))}
-      Total {type} = {currencyFormat(flowList.totalFlow)}
+      <p className='mt-2'>
+        <span className=' text-indigo-200 font-bold'>Total {type}</span> ={' '}
+        {currencyFormat(flowList.totalFlow)}
+      </p>
     </ul>
   );
 };
