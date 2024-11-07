fc4ab28a75451b449bbfbf1bc6ea529a7803be50
GersonEC <gersonenriquez94@gmail.com>
2024-11-07 19:07:25 +0100

    add net flow inn cashflow page

--- src/pages/Cashflow.tsx
+++ src/pages/Cashflow.tsx
@@ -1,8 +1,30 @@
 import { FlowList } from '../components/FlowList';
 import { Nav } from '../components/Nav';
 import { Heading } from '../components/Heading';
+import React from 'react';
+import { currencyFormat } from '../lib/utils';
 
 export const CashFlow = () => {
+  const [netCashflow, setNetCashflow] = React.useState({
+    inflow: 0,
+    outflow: 0,
+    netflow: 0,
+  });
+
+  React.useEffect(() => {
+    const inflowInSessionStorage = sessionStorage.getItem('inflow');
+    const outflowInSessionStorage = sessionStorage.getItem('outflow');
+    if (inflowInSessionStorage && outflowInSessionStorage) {
+      const inflow = JSON.parse(inflowInSessionStorage) as FlowList;
+      const outflow = JSON.parse(outflowInSessionStorage) as FlowList;
+      setNetCashflow({
+        inflow: inflow.totalFlow,
+        outflow: outflow.totalFlow,
+        netflow: inflow.totalFlow - outflow.totalFlow,
+      });
+    }
+  }, []);
+
   return (
     <div>
       <Nav />
@@ -11,6 +33,21 @@ export const CashFlow = () => {
       <FlowList type='inflow' />
       <Heading variant='subtitle'>Outflow</Heading>
       <FlowList type='outflow' />
+      <Heading variant='subtitle'>Net flow</Heading>
+      <p>
+        <span className=' font-semibold text-green-300'>
+          {currencyFormat(netCashflow.inflow)}
+        </span>{' '}
+        -{' '}
+        <span className=' font-semibold text-red-300'>
+          {' '}
+          {currencyFormat(netCashflow.outflow)}
+        </span>{' '}
+        ={' '}
+        <span className=' font-semibold text-blue-400'>
+          {currencyFormat(netCashflow.netflow)}
+        </span>
+      </p>
     </div>
   );
 };
