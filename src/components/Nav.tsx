d66c2cb30764c7159a1abffc3fb6220b5333e02b
GersonEC <gersonenriquez94@gmail.com>
2025-01-23 15:12:46 +0100

    add clerk auth components

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,33 @@
 import { Link, useLocation } from '@tanstack/react-router';
 import { ModeToggle } from './ModeToggle';
 import { Button } from './ui/button';
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from '@clerk/clerk-react';
 
 export const Nav = () => {
   const location = useLocation();
 
   return (
-    <nav className='flex justify-end pb-1 border-b-2'>
-      {location.pathname !== '/' && (
-        <Link to='/bills'>
-          <Button variant={'link'}>Bills</Button>
-        </Link>
-      )}
+    <header>
+      <nav className='flex justify-end pb-1 border-b-2 gap-2'>
+        {location.pathname !== '/' && (
+          <Link to='/bills'>
+            <Button variant={'link'}>Bills</Button>
+          </Link>
+        )}
 
-      <ModeToggle />
-    </nav>
+        <ModeToggle />
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </nav>
+    </header>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { routeTree } from './routes';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { ClerkProvider } from '@clerk/clerk-react';
+
 import './index.css';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
 
 const router = createRouter({ routeTree });
 
@@ -11,8 +19,11 @@ declare module '@tanstack/react-router' {
     router: typeof router;
   }
 }
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </StrictMode>
 );
